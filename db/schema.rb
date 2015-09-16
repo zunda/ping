6a5837136dddc606cc054199ad31cfea5ed9f99b
zunda <zundan@gmail.com>
2015-09-15 17:45:41 -1000

    rake db:migrate

---
+++ db/schema.rb
@@ -0,0 +1,27 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150916031823) do
+
+  create_table "ping_results", force: :cascade do |t|
+    t.float    "lag_ms"
+    t.string   "src_addr"
+    t.string   "dst_addr"
+    t.string   "src_city"
+    t.string   "dst_city"
+    t.float    "distance_km"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+end
