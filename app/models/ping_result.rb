eb52e4f6dc8eefdad0d39f307d5e4af3a53a8f9d
zunda <zundan@gmail.com>
2015-09-15 17:46:22 -1000

    Validate PingResult

--- app/models/ping_result.rb
+++ app/models/ping_result.rb
@@ -1,2 +1,5 @@
 class PingResult < ActiveRecord::Base
+  validates_numericality_of :lag_ms, greater_than_or_equal_to: 0.0
+  validates :src_addr, presence: true, allow_blank: false
+  validates :dst_addr, presence: true, allow_blank: false
 end
--- test/models/ping_result_test.rb
+++ test/models/ping_result_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class PingResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  ValidPingResult = {
+    src_addr: "192.168.1.1",
+    dst_addr: "192.168.1.2",
+    lag_ms: 123.45,
+  }
+
+  test "lag should not be nil" do
+    ping_result = PingResult.new(ValidPingResult.merge(lag_ms: nil))
+    assert_not ping_result.save, "Saved the ping_result without a valid lag"
+  end
+
+  test "lag can be 0" do
+    ping_result = PingResult.new(ValidPingResult.merge(lag_ms: 0.0))
+    assert ping_result.save, "Did not save the ping_result with a 0 lag"
+  end
+
+  test "lag can be more than 0" do
+    ping_result = PingResult.new(ValidPingResult)
+    assert ping_result.save, "Did not save the ping_result with a positive lag"
+  end
+
+  test "src_addr should not be empty" do
+    ping_result = PingResult.new(ValidPingResult.merge(src_addr: nil))
+    assert_not ping_result.save, "Saved the ping_result without a valid src"
+  end
+
+  test "dst_addr should not be empty" do
+    ping_result = PingResult.new(ValidPingResult.merge(dst_addr: nil))
+    assert_not ping_result.save, "Saved the ping_result without a valid dst"
+  end
 end
