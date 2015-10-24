321b4c318f907d99702b537d8d55cd47025f8b97
zunda <zundan@gmail.com>
2015-10-23 18:15:54 -1000

    Implement Location#geocode_from_host
    TODO: convert FQDN into IP address
    
    > l = Location.new(host:'8.8.8.8')
    > l.geocode_from_host
    > l
    => #<Location id: nil, host: "8.8.8.8", latitude: 37.386, longitude: -122.0838, city: "Mountain View, CA 94040, United States", created_at: nil, updated_at: nil>

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,12 @@
 class Location < ActiveRecord::Base
+	def geocode_from_host
+			# TODO: convert FQDN into IP address
+			r = Geocoder.search(host, ip_address: true).first
+			if r
+				self.city = r.address
+				self.latitude = r.latitude
+				self.longitude = r.longitude
+			end
+			return r
+	end
 end
