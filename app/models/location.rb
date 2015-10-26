349517b14464ac349562ceccd416c40413f77c75
zunda <zundan@gmail.com>
2015-10-26 11:40:35 -1000

    Add test for location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,17 @@
 class Location < ActiveRecord::Base
-	def geocode_from_host
-			# TODO: convert FQDN into IP address
-			r = Geocoder.search(host, ip_address: true).first
-			if r
-				self.city = r.address
-				self.latitude = r.latitude
-				self.longitude = r.longitude
-			end
-			return r
-	end
+  @@geocoder = Geocoder
+  def Location::geocoder=(klass)
+    @@geocoder = klass
+  end
+
+  def geocode_from_host!
+      # TODO: convert FQDN into IP address
+      r = @@geocoder.search(host, ip_address: true).first
+      if r
+        self.city = r.address
+        self.latitude = r.latitude
+        self.longitude = r.longitude
+      end
+      return r
+  end
 end
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
+class GeocoderStubError < RuntimeError; end
+class GeocoderStub
+  PreparedResponses = {
+    '8.8.8.8' => {
+      :ip_address => true,
+      :data =>
+        {"dma_code"=>"0", "ip"=>"8.8.8.8", "asn"=>"AS15169", "city"=>"Mountain View", "latitude"=>37.386, "country_code"=>"US", "offset"=>"-7", "country"=>"United States", "region_code"=>"CA", "isp"=>"Google Inc.", "timezone"=>"America/Los_Angeles", "area_code"=>"0", "continent_code"=>"NA", "longitude"=>-122.0838, "region"=>"California", "postal_code"=>"94040", "country_code3"=>"USA"},
+      :error => nil
+    },
+    'www.google.com' => {
+      :ip_address => true,
+      :data => nil,
+      :error => '400 Bad Request'
+    },
+    'Paris, France' => {
+      :ip_address => false,
+      :data =>
+        {"types"=>["locality", "political"], "geometry"=>{"location"=>{"lat"=>48.85341, "lng"=>2.3488}, "location_type"=>"APPROXIMATE", "viewport"=>{"southwest"=>{"lat"=>48.796009, "lng"=>2.216784}, "northeast"=>{"lat"=>48.921822, "lng"=>2.485605}}}, "address_components"=>[{"short_name"=>"Paris", "types"=>["locality", "political"], "long_name"=>"Paris, FR"}, {"short_name"=>"FR", "types"=>["country", "political"], "long_name"=>"France"}]},
+      :error => nil
+    },
+  }
+
+  # result to be used in app/models/location.rb
+  def self.search(query, opts = {})
+    pr = PreparedResponses[query]
+    return [nil] unless pr
+    return [nil] unless (!opts[:ip_address] == !pr[:ip_address])	# !: I want false and nil to be equal here
+    return [nil] if pr[:error]  # Geocoder gem does not raise an error
+    def pr.address
+      data = self[:data]
+      "#{data['city']}, #{data['region_code']} #{data['postal_code']}, #{data['country']}"
+    end
+    def pr.longitude
+      self[:data]['longitude']
+    end
+    def pr.latitude
+      self[:data]['latitude']
+    end
+    return [pr]
+  end
+end
+
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  Location::geocoder = GeocoderStub
+
+  test "looking up an IP address should return a valid location" do
+    l = Location.new(:host => '8.8.8.8')
+    l.geocode_from_host!
+    assert_not_empty l.city
+    assert l.longitude.is_a?(Float), "longitude #{l.longitude.inspect} is not a float"
+    assert l.latitude.is_a?(Float), "latitude #{l.latitude.inspect} is not a float"
+  end
+
+  # TODO: this should return a valid location in the future
+  test "looking up FQDN shuold return a nil" do
+    assert_nil Location.new(:host => 'www.google.com').geocode_from_host!
+  end
 end
