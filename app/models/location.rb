6522580b5ef55722f6ec3a854ae7cddcbb31b323
zunda <zundan@gmail.com>
2015-10-27 18:32:40 -1000

    Refactor app/models/location.rb

--- app/models/location.rb
+++ app/models/location.rb
@@ -19,29 +19,13 @@ class Location < ActiveRecord::Base
       ipaddress = @@resolver.getaddress(ipaddress)
     end
     r = @@geocoder.search(ipaddress, ip_address: true).first
-    if r
-      if not r.address.blank?
-        self.city = r.address
-      elsif not r.city.blank?
-        self.city = r.city
-      end
-      self.latitude = r.latitude
-      self.longitude = r.longitude
-    end
+    obtain_geocode_result(r)
     return r
   end
 
   def geocode_from_city!
     r = @@geocoder.search(city).first
-    if r
-      if not r.address.blank?
-        self.city = r.address
-      elsif not r.city.blank?
-        self.city = r.city
-      end
-      self.latitude = r.latitude
-      self.longitude = r.longitude
-    end
+    obtain_geocode_result(r)
     return r
   end
 
@@ -58,5 +42,18 @@ class Location < ActiveRecord::Base
     return false unless self.latitude.is_a?(Numeric)
     return false unless self.longitude.is_a?(Numeric)
     return true
+  end
+
+  private
+  def obtain_geocode_result(result)
+    if result
+      if not result.address.blank?
+        self.city = result.address
+      elsif not result.city.blank?
+        self.city = result.city
+      end
+      self.latitude = result.latitude
+      self.longitude = result.longitude
+    end
   end
 end
