2ff75022d765bba9d83e22a857e888d4271c4b20
zunda <zundan@gmail.com>
2015-11-24 16:40:52 -1000

    Let frontend obain client id from server

--- app/assets/javascripts/components/dashboard.js.jsx
+++ app/assets/javascripts/components/dashboard.js.jsx
@@ -1,5 +1,30 @@
 var MeasurementBox = React.createClass({
+  loadClientLocation: function() {
+    var path = "/locations/current";
+    $.ajax({
+      url: path,
+      dataType: 'json',
+      cache: true,
+      success: function(data) {
+        this.setState({client: data.id});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(path, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+    this.loadClientLocation();
+  },
   render: function() {
-    return <div className="MeasurementBox">Hello, world!</div>;
+    return (
+      <div className="MeasurementBox">
+        Client id is {this.state.client}.
+      </div>
+    );
   }
 });
