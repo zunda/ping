29109915ba891909d9428da2a7c9a1397384841b
zunda <zundan@gmail.com>
2015-11-26 20:52:40 -1000

    Fix typo

--- test/models/ping_result_test.rb
+++ test/models/ping_result_test.rb
@@ -33,17 +33,17 @@ class PingResultTest < ActiveSupport::TestCase
 
   test "src_addr can be a valid IPv4 address" do
     ping_result = PingResult.new(ValidPingResult.merge(src_addr: '8.8.8.8'))
-    assert ping_result.save, "Did not saved the ping_result with a valid src"
+    assert ping_result.save, "Did not save the ping_result with a valid src"
   end
 
   test "src_addr can be a valid IPv6 address" do
     ping_result = PingResult.new(ValidPingResult.merge(src_addr: '3ffe:505:2::1'))
-    assert ping_result.save, "Did not saved the ping_result with a valid src"
+    assert ping_result.save, "Did not save the ping_result with a valid src"
   end
 
   test "can belong to a location" do
     ping_result = PingResult.new(ValidPingResult.merge(location_id: 1))
     # test/fixtures/locations.yml
-    assert ping_result.save, "Did not saved the ping_result with a valid location"
+    assert ping_result.save, "Did not save the ping_result with a valid location"
   end
 end
