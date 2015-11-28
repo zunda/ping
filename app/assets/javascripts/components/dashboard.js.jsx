a9e67a6011737b2cb8dbd3a3e64268f4366be31b
zunda <zundan@gmail.com>
2015-11-27 15:48:03 -1000

    Show server and client locations

--- app/assets/javascripts/components/dashboard.js.jsx
+++ app/assets/javascripts/components/dashboard.js.jsx
@@ -1,48 +1,65 @@
-var MeasurementBox = React.createClass({
-  loadLocation: function(path, prop) {
-    var state = new Object();
-    $.ajax({
-      url: path,
-      dataType: 'json',
-      cache: true,
-      success: function(data) {
-        state[prop] = data.id
-        this.setState(state);
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(path, status, err.toString());
-      }.bind(this)
-    });
-  },
-  loadClientLocation: function() {
-    this.loadLocation("/locations/current", 'client');
-  },
-  loadServerLocation: function() {
-    this.loadLocation("/locations/server", 'server');
-  },
+var loadLocation = function(path) {
+  $.ajax({
+    url: path,
+    dataType: 'json',
+    cache: true,
+    success: function(data) {
+      this.setState({id: data.id, city: data.city});
+    }.bind(this),
+    error: function(xhr, status, err) {
+      console.error(path, status, err.toString());
+    }.bind(this)
+  });
+};
 
+var MeasurementBox = React.createClass({
+  render: function() {
+    return (
+      <div className="measurementBox">
+        <ServerBox />
+        <ClientBox />
+      </div>
+    );
+  }
+});
+
+var ClientBox = React.createClass({
   getInitialState: function() {
     return {};
   },
   componentDidMount: function() {
-    this.loadClientLocation();
-    this.loadServerLocation();
+    loadLocation.call(this, "/locations/current");
   },
+
   render: function() {
+    var city = this.state.city;
+    if (!city) {
+      city = "unknown location";
+    }
     return (
-      <div className="measurementBox">
-        Client id is {this.state.client}.
-        <ServerBox data={this.state.server} />
+      <div className="clientBox">
+        Your are at {city}.
       </div>
     );
   }
 });
 
 var ServerBox = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+    loadLocation.call(this, "/locations/server");
+  },
+
   render: function() {
+    var city = this.state.city;
+    if (!city) {
+      city = "unknown location";
+    }
     return (
       <div className="serverBox">
-        Server id is {this.props.data}.
+        Ping the server at {city}!
       </div>
     );
   }
