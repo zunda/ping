88023a59adac5c7c9ca8e17c427aa872569a714b
zunda <zundan@gmail.com>
2015-11-27 17:06:46 -1000

    Hand location IDs to PingBox

--- app/assets/javascripts/components/dashboard.js.jsx
+++ app/assets/javascripts/components/dashboard.js.jsx
@@ -1,41 +1,49 @@
-var loadLocation = function(path) {
-  $.ajax({
-    url: path,
-    dataType: 'json',
-    cache: true,
-    success: function(data) {
-      if (data.city) {
-        this.setState({id: data.id, city: data.city});
-      } else {
-        setTimeout(loadLocation.bind(this), 2000, path);
-      }
-    }.bind(this),
-    error: function(xhr, status, err) {
-      console.error(path, status, err.toString());
-    }.bind(this)
-  });
-};
-
 var MeasurementBox = React.createClass({
+  interval: 2000,
+  loadLocation: function(path, propPrefix) {
+    $.ajax({
+      url: path,
+      dataType: 'json',
+      cache: true,
+      success: function(data) {
+        if (data.city) {
+          var newState = {};
+          newState[propPrefix + "Id"] = data.id;
+          newState[propPrefix + "City"] = data.city;
+          this.setState(newState);
+        } else {
+          setTimeout(loadLocation.bind(this), this.interval, path, propPrefix);
+        }
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(path, status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+    this.loadLocation("/locations/current", "client");
+    this.loadLocation("/locations/server", "server");
+  },
+
   render: function() {
     return (
       <div className="measurementBox">
-        Ping the server at <ServerCity /> from <ClientCity />!
+        <p>
+        Ping the server at <ServerCity city={this.state.serverCity} /> from <ClientCity city={this.state.clientCity} />!
+        </p>
+        <PingBox serverId={this.state.serverId} clientId={this.state.clientId} />
       </div>
     );
   }
 });
 
 var ClientCity = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    loadLocation.call(this, "/locations/current");
-  },
-
   render: function() {
-    var city = this.state.city;
+    var city = this.props.data;
     if (!city) {
       city = "unknown location";
     }
@@ -46,20 +54,23 @@ var ClientCity = React.createClass({
 });
 
 var ServerCity = React.createClass({
-  getInitialState: function() {
-    return {};
-  },
-  componentDidMount: function() {
-    loadLocation.call(this, "/locations/server");
-  },
-
   render: function() {
-    var city = this.state.city;
+    var city = this.props.data;
     if (!city) {
       city = "unknown location";
     }
     return (
       <span className="serverCity">{city}</span>
+    );
+  }
+});
+
+var PingBox = React.createClass({
+  render: function() {
+    var serverId = this.props.serverId;
+    var clientId = this.props.clientId;
+    return (
+      <div className="PingBox">Pinging from location id:{clientId} to id:{serverId}</div>
     );
   }
 });
