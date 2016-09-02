48cf3ea496e89ed64e23dda9d74720f4db0836be
zunda <zundan@gmail.com>
2016-09-01 18:25:21 -1000

    Record request.remote_ip

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class DashboardController < ApplicationController
   def index
     logger.info "X-Forwarded-For: #{request.headers['X-Forwarded-For'].inspect}"
+    logger.info "request.remote_ip: #{request.remote_ip.inspect}"
   end
 end
