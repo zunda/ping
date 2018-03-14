723da33bb196d95bfa774ac7032f100943ccda07
zunda <zundan@gmail.com>
2018-03-13 16:47:26 -1000

    Try not force ssl on defaulit domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,13 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = {
+    redirect: {
+      exclude: -> request {
+        request.host =~ /\.herokuapp\.com\Z/
+      }
+    }
+  }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
